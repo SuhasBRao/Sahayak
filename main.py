1f77ae0e472482f7f39c7c7b8e50a8ea450673df
@SuhasBRao@ <raos04567@gmail.com>
2022-03-11 16:07:56 +0530

    added a functionality to run continuosly

--- main.py
+++ main.py
@@ -13,18 +13,37 @@ def speak(text):
 def listen():
     with sr.Microphone() as source:
         # read the audio data from the default microphone
-        audio_data = r.record(source, duration=5)
-        print("Recognizing...")
-        # convert speech to text
-        text = r.recognize_google(audio_data, language= 'en-IN')
-        print(text)
+        # audio_data = r.record(source, duration=5)
         
+        r.adjust_for_ambient_noise(source, duration= 0.2)
+        
+        try:
+            print('Say something.......')
+            audio_data = r.listen(source, timeout= 10)
+            print("Recognizing...")
+            text = r.recognize_google(audio_data, language= 'en-IN')
+            text = text.lower()
+            return text
+        # except sr.UnknownValueError:
+        #     print('Not able to recognise')
+        except sr.WaitTimeoutError :
+            return 0
+        # convert speech to text
+        
+        # print(text)
 # {
 # Driver Code starts
 if __name__ == "__main__":
     # write your code here
     # listen()
     speak('Hi, How may i help you?')
-    listen()
+    # text = listen()
+    while(1):
+        text = listen()
+        if text == 0 or None:
+            continue
+        else:
+            print(text)
+    print('no audio')
 
 # } Driver Code ends
