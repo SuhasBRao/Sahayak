98c4d7a1a746704078248934ce1ceb1fc5345d34
@SuhasBRao@ <raos04567@gmail.com>
2022-03-12 16:49:25 +0530

    trying new feature

--- main.py
+++ main.py
@@ -25,15 +25,18 @@ def listen():
             audio_data = r.listen(source, timeout= 10)
             print("Recognizing...")
             text = r.recognize_google(audio_data, language= 'en-IN')
-            # text = text.lower()
             return text.lower()
+        
         except sr.UnknownValueError:
             print('Not able to recognise')
+            
         except sr.WaitTimeoutError :
             return 0
-        
-        # print(text)
 
+          
+def thank_you_response():
+    speak('Your Welocme, Master!!')
+    
 def getdate():
     now = datetime.now()
     date = now.strftime("%d")
@@ -43,38 +46,64 @@ def getdate():
     
     year = now.strftime("%y")
     print(f'{date}/{month}/{year}')
-    print(month_name)
+    # print(month_name)
     
-    return date, month_name, year
-
+    speak("Today's date is {0} of {1} {2} ".format(date, month_name, year))
+            
 def getTime():
     now = datetime.now()
-    dt_time = now.strftime("%H:%M:%S")
-    return dt_time
+    cur_time = now.strftime("%H:%M:%S")
     
-    
+    speak(f'Current time is {cur_time}')
+                
+
+
 # {
 # Driver Code starts
 if __name__ == "__main__":
     # write your code here
-    # listen()
-    speak('Hi, How may i help you?')
-    # text = listen()
+    
+    # speak('Hi, How may i help you?')
+    
     # allows the program to run continously
     while(1):
         text = listen()
         print(text)
+        
         if text == 0 or text == None:
             continue
-        if text != None or text != 0:
-            if 'date' in text:
-                date,month, year = getdate()
-                speak("Today's date is {0} of {1} {2} ".format(date, month, year))
-            if 'time' in text:
-                pass
-            if 'thank'
-        else:
-            print(text)
+        
+        if 'sahayak' in text:
+
+            text = text.replace('sahayak', "")
+            
+            key_words_list = ["today's date", 'thankyou']
+            
+            
+            res = list(map(lambda x: x in text, key_words_list))
+            
+            print(res)
+            # {'hello sahayak': speak('Hello master, how can i help you'),
+            # func = {"today's date": getdate,
+            #  ("thank you", 'thankyou'): thank_you_response,
+            #  'current time': getTime}
+            
+            # func
+             
+            
+            
+            
+        # else:
+            # if 'hello sahayak' in text:
+            #     speak('Hey master, how can i help you ?')
+                
+            # if "today's date" in text:
+            #     pass
+            # if 'current time' in text:
+            #     pass
+                
+            # if 'thank you' in text or 'thankyou' in text:
+            #     pass
     print('no audio')
 
 # } Driver Code ends
