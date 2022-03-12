62938ae10b12b1b0f64b2ecea50def929706867f
@SuhasBRao@ <raos04567@gmail.com>
2022-03-12 15:48:34 +0530

    adding time and date finding function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pyttsx3 
+from calendar import month
+import pyttsx3
+from datetime import datetime 
 import speech_recognition as sr
+from webob import year
 
 r = sr.Recognizer()
 
@@ -18,19 +21,38 @@ def listen():
         r.adjust_for_ambient_noise(source, duration= 0.2)
         
         try:
-            print('Say something.......')
+            print('listening.......')
             audio_data = r.listen(source, timeout= 10)
             print("Recognizing...")
             text = r.recognize_google(audio_data, language= 'en-IN')
-            text = text.lower()
-            return text
-        # except sr.UnknownValueError:
-        #     print('Not able to recognise')
+            # text = text.lower()
+            return text.lower()
+        except sr.UnknownValueError:
+            print('Not able to recognise')
         except sr.WaitTimeoutError :
             return 0
-        # convert speech to text
         
         # print(text)
+
+def getdate():
+    now = datetime.now()
+    date = now.strftime("%d")
+    month = now.strftime("%m")
+    
+    month_name = now.strftime('%B')
+    
+    year = now.strftime("%y")
+    print(f'{date}/{month}/{year}')
+    print(month_name)
+    
+    return date, month_name, year
+
+def getTime():
+    now = datetime.now()
+    dt_time = now.strftime("%H:%M:%S")
+    return dt_time
+    
+    
 # {
 # Driver Code starts
 if __name__ == "__main__":
@@ -38,10 +60,19 @@ if __name__ == "__main__":
     # listen()
     speak('Hi, How may i help you?')
     # text = listen()
+    # allows the program to run continously
     while(1):
         text = listen()
-        if text == 0 or None:
+        print(text)
+        if text == 0 or text == None:
             continue
+        if text != None or text != 0:
+            if 'date' in text:
+                date,month, year = getdate()
+                speak("Today's date is {0} of {1} {2} ".format(date, month, year))
+            if 'time' in text:
+                pass
+            if 'thank'
         else:
             print(text)
     print('no audio')
